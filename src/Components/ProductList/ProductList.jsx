b9dc8772cc5ceac82a32dc1beca576ecc06f4651
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-12 01:43:23 +0500

    init

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -39,7 +39,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    },[])
+    },[addedItems])
 
     useEffect(()=> {
         tg.onEvent('mainButtonClicked', onSendData)
