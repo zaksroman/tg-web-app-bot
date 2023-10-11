d04492818d963a3dcce72f07d82d7d7e039bedb2
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-11 19:12:22 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import {useEffect} from "react";
 
-const tg = window.Telegram.WebApp
+
 function App() {
 
   useEffect(()=> {
     tg.ready()
   }, [])
-  const onClose = () => {
-      tg.close()
-  }
+
 
   return (
     <div className="App">
