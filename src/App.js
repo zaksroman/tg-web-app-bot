5273682a810e9a86f439ea2eb5319e1d2e5023fa
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-11 20:50:30 +0500

    init

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         <Routes>
             <Route index element={<ProductList/>}/>
             <Route path={'form'} element={<Form/>}/>
+            {/*<Route index element={<Form/>}/>*/}
         </Routes>
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css'
 import Button from "../Button/Button";
 import {useTelegram} from "../hooks/useTelegram";
+
 const Header = () => {
 
     const {user, onClose} = useTelegram()
