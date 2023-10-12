35f3d38b13b1f3c5e033692e13e7e421dddb77cd
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-12 17:05:52 +0500

    init

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -32,7 +32,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000/web-data', {
+        fetch('http://45.140.179.236:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
