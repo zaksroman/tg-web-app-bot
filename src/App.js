650e404c9657a6dc56a68df5146f0a0270a19464
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-17 23:03:56 +0500

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Routes, Route} from "react-router-dom";
 import ProductList from "./Components/ProductList/ProductList";
 import BigProoductItem from "./Components/ProductItem/BigItem/BigProoductItem";
 import Basket from "./Components/Basket/Basket";
-import {useSelector} from "react-redux";
 function App() {
   const {tg} = useTelegram()
 
@@ -19,7 +18,7 @@ function App() {
         <link rel="stylesheet" href=""/>
         <Routes>
             <Route index element={<ProductList/>}/>
-            <Route path={'/bigproructitem/:id'} element={ <BigProoductItem/>}}/>
+            <Route path={'/bigproructitem/:id'} element={ <BigProoductItem/>}/>
             <Route path={'/basket'} element={<Basket/>}/>
         </Routes>
     </div>
