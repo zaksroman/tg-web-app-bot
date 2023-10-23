48e0faabfcc2d96e4c4794e3cc15651c23a5bc73
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-23 20:33:26 +0500

    init

--- src/Components/ProductItem/BigItem/Circle.jsx
+++ src/Components/ProductItem/BigItem/Circle.jsx
@@ -1,25 +1,34 @@
-import Swipeble from 'react-swipeable';
+import {useSwipeable} from 'react-swipeable';
 import React, { useState } from 'react';
 
-const photos = ['https://gas-kvas.com/uploads/posts/2023-02/1675489758_gas-kvas-com-p-izobrazheniya-i-kartinki-na-fonovii-risuno-41.jpg', 'https://s1.1zoom.ru/big3/474/354282-admin.jpg'  ]
 const Circle = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
+    const photos = ['https://gas-kvas.com/uploads/posts/2023-02/1675489758_gas-kvas-com-p-izobrazheniya-i-kartinki-na-fonovii-risuno-41.jpg', 'https://s1.1zoom.ru/big3/474/354282-admin.jpg'  ]
+    const [currentSlide, setCurrentSlide] = useState(0); // индекс текущей фотографии
 
-    const handleSwipe = (direction) => {
-        if (direction === 'left') {
+
+    const handleSwipeLeft = () => {
+        if (currentSlide < photos.length - 1) {
             setCurrentSlide(currentSlide + 1);
-        } else if (direction === 'right') {
+        }
+    };
+
+    const handleSwipeRight = () => {
+        if (currentSlide > 0) {
             setCurrentSlide(currentSlide - 1);
         }
     };
 
+    const handlers = useSwipeable({
+        onSwipedLeft: handleSwipeLeft,
+        onSwipedRight: handleSwipeRight,
+        preventDefaultTouchmoveEvent: true, // предотвратить прокрутку страницы свайпом
+        trackMouse: true // допустить использование мыши на компьютере
+    });
+
     return (
-        <Swipeble
-            onSwipedLeft={() => handleSwipe('left')}
-            onSwipedRight={() => handleSwipe('right')}
-        >
+        <div {...handlers}>
             <img src={photos[currentSlide]} alt="carousel" />
-        </Swipeble>
+        </div>
     );
 };
 
