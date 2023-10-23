0e6685ea4067b3a03d2b1607f8f42890f9c3b13a
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-23 21:54:27 +0500

    init

--- src/Components/ProductItem/BigItem/Circle.jsx
+++ src/Components/ProductItem/BigItem/Circle.jsx
@@ -5,18 +5,15 @@ import './Circle.css'
 const Circle = () => {
     const photos = ['https://appstorrent.ru/uploads/fotos/foto_90458.jpg', 'https://sun9-69.userapi.com/s/v1/ig2/gcOWJCE0-Ev3SVdrgkmyR-WuWIzpgljePQGjmaX0oq_vFTt-K-sOOUjoEdLf-VQurGL7XqheFlc_fhQMTc1PQGtV.jpg?size=200x200&quality=95&crop=0,0,841,841&ava=1', 'https://cosplay2.ru/files/453/logo.png?preview&size=537x'];
     const [currentSlide, setCurrentSlide] = useState(0);
-    const [prevSlide, setPrevSlide] = useState(currentSlide);
 
     const handleSwipeLeft = () => {
         if (currentSlide < photos.length - 1) {
-            setPrevSlide(currentSlide);
             setCurrentSlide(currentSlide + 1);
         }
     };
 
     const handleSwipeRight = () => {
         if (currentSlide > 0) {
-            setPrevSlide(currentSlide);
             setCurrentSlide(currentSlide - 1);
         }
     };
@@ -41,6 +38,15 @@ const Circle = () => {
             {currentSlide < photos.length - 1 && (
                 <div className="carouse-swipe-area-right" onClick={handleSwipeLeft}></div>
             )}
+            <div className="indicators">
+                {photos.map((_, index) => (
+                    <div
+                        key={index}
+                        className={`indicator ${index === currentSlide ? 'active' : ''}`}
+                        onClick={() => setCurrentSlide(index)}
+                    ></div>
+                ))}
+            </div>
         </div>
     );
 };
