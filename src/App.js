ece225b5d82a67532912f215879f17bb18eba2ce
Zakshevskiy Roman <zaksroman@gmail.com>
2023-10-23 22:15:31 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./Components/hooks/useTelegram";
-import {Routes, Route, useNavigate} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import ProductList from "./Components/ProductList/ProductList";
 import BigProoductItem from "./Components/ProductItem/BigItem/BigProoductItem";
 import Basket from "./Components/Basket/Basket";
-import {useSelector} from "react-redux";
 import PersonalData from "./Components/PersaanalData/PersonalData";
 
 function App() {
   const {tg} = useTelegram()
-  const addedItems = useSelector(state => state.addedItems);
-  const navigate = useNavigate();
 
   useEffect(()=> {
     tg.ready()
     tg.expand()
     tg.isClosingConfirmationEnabled(true)
   }, [])
-
-
-
-  // const handleClick = () => {
-  //       navigate('/basket');
-  // }
-  //
-  // useEffect(()=> {
-  //       tg.onEvent('mainButtonClicked', handleClick)
-  //       return () => {
-  //           tg.offEvent('mainButtonClicked', handleClick)
-  //       }
-  // }, [handleClick])
-  //
-  //
-  // if (addedItems.length === 0) {
-  //       tg.MainButton.hide()
-  // } else {
-  //       tg.MainButton.show()
-  //       tg.MainButton.setParams({
-  //           text: `Перейти в корзину`
-  //   })
-  // }
 
 
   return (
